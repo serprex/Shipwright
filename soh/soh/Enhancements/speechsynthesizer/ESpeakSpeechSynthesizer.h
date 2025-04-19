7e065ac8d778770e4dba903f93076a254e8f81e6
Demur Rumed <junkmute@hotmail.com>
2025-04-19 16:15:30 +0000

    dlopen

--- soh/soh/Enhancements/speechsynthesizer/ESpeakSpeechSynthesizer.h
+++ soh/soh/Enhancements/speechsynthesizer/ESpeakSpeechSynthesizer.h
@@ -2,6 +2,18 @@
 
 #include "SpeechSynthesizer.h"
 
+extern "C" {
+#include <espeak-ng/speak_lib.h>
+
+// C23 typeof could help here
+typedef ESPEAK_API int (*speak_Initialize)(espeak_AUDIO_OUTPUT output, int buflength, const char* path, int options);
+typedef ESPEAK_API espeak_ERROR (*speak_Terminate)(void);
+typedef ESPEAK_API espeak_ERROR (*speak_SetVoiceByProperties)(espeak_VOICE* voice_spec);
+typedef ESPEAK_API espeak_ERROR (*speak_Synth)(const void* text, size_t size, unsigned int position,
+                                               espeak_POSITION_TYPE position_type, unsigned int end_position,
+                                               unsigned int flags, unsigned int* unique_identifier, void* user_data);
+}
+
 class ESpeakSpeechSynthesizer : public SpeechSynthesizer {
   public:
     ESpeakSpeechSynthesizer();
@@ -13,5 +25,10 @@ class ESpeakSpeechSynthesizer : public SpeechSynthesizer {
     void DoUninitialize(void);
 
   private:
-    const char* mLanguage;
+    const char* mLanguage = NULL;
+    void* espeak = NULL;
+    speak_Initialize Initialize = NULL;
+    speak_SetVoiceByProperties SetVoiceByProperties = NULL;
+    speak_Synth Synth = NULL;
+    speak_Terminate Terminate = NULL;
 };
