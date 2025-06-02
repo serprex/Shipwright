1ca27ba3bad7cfea2592d1c3ebc9a91ba617b6ab
Demur Rumed <junkmute@hotmail.com>
2025-06-02 02:40:46 +0000

    revert forcing rando to at least 1

--- soh/soh/Enhancements/CuccosToReturn.cpp
+++ soh/soh/Enhancements/CuccosToReturn.cpp
@@ -2,8 +2,6 @@
 #include "soh/ShipInit.hpp"
 #include "soh/Enhancements/randomizer/context.h"
 
-#define RAND_GET_OPTION(option) Rando::Context::GetInstance()->GetOption(option).Get()
-
 extern "C" {
 extern PlayState* gPlayState;
 #include "src/overlays/actors/ovl_En_Niw_Lady/z_en_niw_lady.h"
@@ -14,10 +12,6 @@ void RegisterCuccosToReturn() {
         EnNiwLady* enNiwLady = va_arg(args, EnNiwLady*);
         // Override starting Cucco count using setting value
         enNiwLady->cuccosInPen = 7 - CVarGetInteger(CVAR_ENHANCEMENT("CuccosToReturn"), 7);
-        if (IS_RANDO && enNiwLady->cuccosInPen == 7) {
-            // force at least 1 in rando to test str0
-            enNiwLady->cuccosInPen = 6;
-        }
         *should = false;
     });
 }
