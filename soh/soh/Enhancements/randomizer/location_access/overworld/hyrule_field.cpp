phobos2390/OOT_SoH

rocks, boulders

soh/soh/Enhancements/randomizer/location_access/overworld/hyrule_field.cpp
@@ -61,6 +61,22 @@ void RegionTable_Init_HyruleField() {
         LOCATION(RC_HF_NEAR_KF_GRASS_10,       logic->CanCutShrubs()),
         LOCATION(RC_HF_NEAR_KF_GRASS_11,       logic->CanCutShrubs()),
         LOCATION(RC_HF_NEAR_KF_GRASS_12,       logic->CanCutShrubs()),
+        LOCATION(RC_HF_SILVER_BOULDER,         logic->CanUse(RG_SILVER_GAUNTLETS)),
+        LOCATION(RC_HF_ROCK_1,                 true),
+        LOCATION(RC_HF_ROCK_2,                 true),
+        LOCATION(RC_HF_ROCK_3,                 true),
+        LOCATION(RC_HF_ROCK_4,                 true),
+        LOCATION(RC_HF_ROCK_5,                 true),
+        LOCATION(RC_HF_ROCK_6,                 true),
+        LOCATION(RC_HF_ROCK_7,                 true),
+        LOCATION(RC_HF_ROCK_8,                 true),
+        LOCATION(RC_HF_BOULDER_1,              logic->BlastOrSmash()),
+        LOCATION(RC_HF_BOULDER_2,              logic->BlastOrSmash()),
+        LOCATION(RC_HF_BOULDER_3,              logic->BlastOrSmash()),
+        LOCATION(RC_HF_BRONZE_BOULDER_1,       logic->IsAdult && logic->CanUse(RG_MEGATON_HAMMER)),
+        LOCATION(RC_HF_BRONZE_BOULDER_2,       logic->IsAdult && logic->CanUse(RG_MEGATON_HAMMER)),
+        LOCATION(RC_HF_BRONZE_BOULDER_3,       logic->IsAdult && logic->CanUse(RG_MEGATON_HAMMER)),
+        LOCATION(RC_HF_BRONZE_BOULDER_4,       logic->IsAdult && logic->CanUse(RG_MEGATON_HAMMER)),
     }, {
         //Exits
         Entrance(RR_LW_BRIDGE,              []{return true;}),

serprex/Shipwright

soh/soh/Enhancements/randomizer/location_access/overworld/hyrule_field.cpp
@@ -61,6 +61,22 @@ void RegionTable_Init_HyruleField() {
         LOCATION(RC_HF_NEAR_KF_GRASS_10,       logic->CanCutShrubs()),
         LOCATION(RC_HF_NEAR_KF_GRASS_11,       logic->CanCutShrubs()),
         LOCATION(RC_HF_NEAR_KF_GRASS_12,       logic->CanCutShrubs()),
+        LOCATION(RC_HF_SILVER_BOULDER,         logic->CanUse(RG_SILVER_GAUNTLETS)),
+        LOCATION(RC_HF_ROCK_1,                 true),
+        LOCATION(RC_HF_ROCK_2,                 true),
+        LOCATION(RC_HF_ROCK_3,                 true),
+        LOCATION(RC_HF_ROCK_4,                 true),
+        LOCATION(RC_HF_ROCK_5,                 true),
+        LOCATION(RC_HF_ROCK_6,                 true),
+        LOCATION(RC_HF_ROCK_7,                 true),
+        LOCATION(RC_HF_ROCK_8,                 true),
+        LOCATION(RC_HF_BOULDER_NORTH,          logic->BlastOrSmash()),
+        LOCATION(RC_HF_BOULDER_BY_MARKET,      logic->BlastOrSmash()),
+        LOCATION(RC_HF_BOULDER_SOUTH,          logic->BlastOrSmash()),
+        LOCATION(RC_HF_BRONZE_BOULDER_1,       logic->IsAdult && logic->CanUse(RG_MEGATON_HAMMER)),
+        LOCATION(RC_HF_BRONZE_BOULDER_2,       logic->IsAdult && logic->CanUse(RG_MEGATON_HAMMER)),
+        LOCATION(RC_HF_BRONZE_BOULDER_3,       logic->IsAdult && logic->CanUse(RG_MEGATON_HAMMER)),
+        LOCATION(RC_HF_BRONZE_BOULDER_4,       logic->IsAdult && logic->CanUse(RG_MEGATON_HAMMER)),
     }, {
         //Exits
         Entrance(RR_LW_BRIDGE,              []{return true;}),