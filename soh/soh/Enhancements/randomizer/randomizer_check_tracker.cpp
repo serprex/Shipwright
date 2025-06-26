ccd0b3154219bc24e731d74db3f22ceeaa33fd0e
Demur Rumed <junkmute@hotmail.com>
2025-06-26 05:17:08 +0000

    format

--- soh/soh/Enhancements/randomizer/randomizer_check_tracker.cpp
+++ soh/soh/Enhancements/randomizer/randomizer_check_tracker.cpp
@@ -1600,28 +1600,27 @@ bool IsCheckShuffled(RandomizerCheck rc) {
                (loc->GetRCType() != RCTYPE_COW || showCows) &&
                (loc->GetRCType() != RCTYPE_FISH ||
                 OTRGlobals::Instance->gRandoContext->GetFishsanity()->GetFishLocationIncluded(loc)) &&
-                   (loc->GetRCType() != RCTYPE_FREESTANDING ||
-                    (showOverworldFreestanding && RandomizerCheckObjects::AreaIsOverworld(loc->GetArea())) ||
-                    (showDungeonFreestanding && RandomizerCheckObjects::AreaIsDungeon(loc->GetArea()))) &&
-                   (loc->GetRCType() != RCTYPE_ADULT_TRADE || showAdultTrade ||
-                    rc == RC_KAK_ANJU_AS_ADULT ||  // adult trade checks that are always shuffled
-                    rc == RC_DMT_TRADE_CLAIM_CHECK // even when shuffle adult trade is off
-                    ) &&
-                   (rc != RC_KF_KOKIRI_SWORD_CHEST || showKokiriSword) &&
-                   (rc != RC_TOT_MASTER_SWORD || showMasterSword) && (rc != RC_LH_HYRULE_LOACH || showHyruleLoach) &&
-                   (rc != RC_ZR_MAGIC_BEAN_SALESMAN || showBeans) && (rc != RC_HC_MALON_EGG || showWeirdEgg) &&
-                   (loc->GetRCType() != RCTYPE_FROG_SONG || showFrogSongRupees) &&
-                   ((loc->GetRCType() != RCTYPE_MAP && loc->GetRCType() != RCTYPE_COMPASS) ||
-                    showStartingMapsCompasses) &&
-                   (loc->GetRCType() != RCTYPE_FAIRY || showFairies) &&
-                   (loc->GetRCType() != RCTYPE_SMALL_KEY || showKeysanity) &&
-                   (loc->GetRCType() != RCTYPE_BOSS_KEY || showBossKeysanity) &&
-                   (loc->GetRCType() != RCTYPE_GANON_BOSS_KEY || showGanonBossKey) &&
-                   (rc != RC_KAK_100_GOLD_SKULLTULA_REWARD || show100SkullReward) &&
-                   (loc->GetRCType() != RCTYPE_GF_KEY && rc != RC_TH_FREED_CARPENTERS ||
-                    (showGerudoCard && rc == RC_TH_FREED_CARPENTERS) ||
-                    (fortressNormal && showGerudoFortressKeys && loc->GetRCType() == RCTYPE_GF_KEY) ||
-                    (fortressFast && showGerudoFortressKeys && rc == RC_TH_1_TORCH_CARPENTER));
+               (loc->GetRCType() != RCTYPE_FREESTANDING ||
+                (showOverworldFreestanding && RandomizerCheckObjects::AreaIsOverworld(loc->GetArea())) ||
+                (showDungeonFreestanding && RandomizerCheckObjects::AreaIsDungeon(loc->GetArea()))) &&
+               (loc->GetRCType() != RCTYPE_ADULT_TRADE || showAdultTrade ||
+                rc == RC_KAK_ANJU_AS_ADULT ||  // adult trade checks that are always shuffled
+                rc == RC_DMT_TRADE_CLAIM_CHECK // even when shuffle adult trade is off
+                ) &&
+               (rc != RC_KF_KOKIRI_SWORD_CHEST || showKokiriSword) && (rc != RC_TOT_MASTER_SWORD || showMasterSword) &&
+               (rc != RC_LH_HYRULE_LOACH || showHyruleLoach) && (rc != RC_ZR_MAGIC_BEAN_SALESMAN || showBeans) &&
+               (rc != RC_HC_MALON_EGG || showWeirdEgg) &&
+               (loc->GetRCType() != RCTYPE_FROG_SONG || showFrogSongRupees) &&
+               ((loc->GetRCType() != RCTYPE_MAP && loc->GetRCType() != RCTYPE_COMPASS) || showStartingMapsCompasses) &&
+               (loc->GetRCType() != RCTYPE_FAIRY || showFairies) &&
+               (loc->GetRCType() != RCTYPE_SMALL_KEY || showKeysanity) &&
+               (loc->GetRCType() != RCTYPE_BOSS_KEY || showBossKeysanity) &&
+               (loc->GetRCType() != RCTYPE_GANON_BOSS_KEY || showGanonBossKey) &&
+               (rc != RC_KAK_100_GOLD_SKULLTULA_REWARD || show100SkullReward) &&
+               (loc->GetRCType() != RCTYPE_GF_KEY && rc != RC_TH_FREED_CARPENTERS ||
+                (showGerudoCard && rc == RC_TH_FREED_CARPENTERS) ||
+                (fortressNormal && showGerudoFortressKeys && loc->GetRCType() == RCTYPE_GF_KEY) ||
+                (fortressFast && showGerudoFortressKeys && rc == RC_TH_1_TORCH_CARPENTER));
     } else if (loc->IsVanillaCompletion()) {
         return (OTRGlobals::Instance->gRandoContext->IsQuestOfLocationActive(rc) || rc == RC_GIFT_FROM_RAURU) &&
                rc != RC_LINKS_POCKET;
