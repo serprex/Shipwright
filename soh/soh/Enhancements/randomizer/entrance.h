b6c39bc28b0d7661fc3756ec2ca00aec8343460d
Demur Rumed <junkmute@hotmail.com>
2025-07-19 22:13:15 +0000

    post rebase mixups

--- soh/soh/Enhancements/randomizer/entrance.h
+++ soh/soh/Enhancements/randomizer/entrance.h
@@ -9,8 +9,6 @@
 #define ENTRANCE_SHUFFLE_SUCCESS 0
 #define ENTRANCE_SHUFFLE_FAILURE 1
 
-constexpr std::string CleanCheckConditionString(std::string condition);
-
 namespace Rando {
 enum class EntranceType {
     None,
