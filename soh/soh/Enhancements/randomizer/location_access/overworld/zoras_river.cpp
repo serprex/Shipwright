phobos2390/OOT_SoH

rocks, boulders

soh/soh/Enhancements/randomizer/location_access/overworld/zoras_river.cpp
@@ -7,19 +7,23 @@ void RegionTable_Init_ZoraRiver() {
     // clang-format off
     areaTable[RR_ZR_FRONT] = Region("ZR Front", "Zora River", {RA_ZORAS_RIVER}, DAY_NIGHT_CYCLE, {}, {
         //Locations
-        LOCATION(RC_ZR_GS_TREE,  logic->IsChild && logic->CanKillEnemy(RE_GOLD_SKULLTULA, ED_CLOSE)),
-        LOCATION(RC_ZR_GRASS_1,  logic->CanCutShrubs()),
-        LOCATION(RC_ZR_GRASS_2,  logic->CanCutShrubs()),
-        LOCATION(RC_ZR_GRASS_3,  logic->CanCutShrubs()),
-        LOCATION(RC_ZR_GRASS_4,  logic->CanCutShrubs()),
-        LOCATION(RC_ZR_GRASS_5,  logic->CanCutShrubs()),
-        LOCATION(RC_ZR_GRASS_6,  logic->CanCutShrubs()),
-        LOCATION(RC_ZR_GRASS_7,  logic->CanCutShrubs()),
-        LOCATION(RC_ZR_GRASS_8,  logic->CanCutShrubs()),
-        LOCATION(RC_ZR_GRASS_9,  logic->CanCutShrubs()),
-        LOCATION(RC_ZR_GRASS_10, logic->CanCutShrubs()),
-        LOCATION(RC_ZR_GRASS_11, logic->CanCutShrubs()),
-        LOCATION(RC_ZR_GRASS_12, logic->CanCutShrubs()),
+        LOCATION(RC_ZR_GS_TREE,   logic->IsChild && logic->CanKillEnemy(RE_GOLD_SKULLTULA, ED_CLOSE)),
+        LOCATION(RC_ZR_GRASS_1,   logic->CanCutShrubs()),
+        LOCATION(RC_ZR_GRASS_2,   logic->CanCutShrubs()),
+        LOCATION(RC_ZR_GRASS_3,   logic->CanCutShrubs()),
+        LOCATION(RC_ZR_GRASS_4,   logic->CanCutShrubs()),
+        LOCATION(RC_ZR_GRASS_5,   logic->CanCutShrubs()),
+        LOCATION(RC_ZR_GRASS_6,   logic->CanCutShrubs()),
+        LOCATION(RC_ZR_GRASS_7,   logic->CanCutShrubs()),
+        LOCATION(RC_ZR_GRASS_8,   logic->CanCutShrubs()),
+        LOCATION(RC_ZR_GRASS_9,   logic->CanCutShrubs()),
+        LOCATION(RC_ZR_GRASS_10,  logic->CanCutShrubs()),
+        LOCATION(RC_ZR_GRASS_11,  logic->CanCutShrubs()),
+        LOCATION(RC_ZR_GRASS_12,  logic->CanCutShrubs()),
+        LOCATION(RC_ZR_BOULDER_1, logic->IsChild && logic->BlastOrSmash()),
+        LOCATION(RC_ZR_BOULDER_2, logic->IsChild && logic->BlastOrSmash()),
+        LOCATION(RC_ZR_BOULDER_3, logic->IsChild && logic->BlastOrSmash()),
+        LOCATION(RC_ZR_BOULDER_4, logic->IsChild && logic->BlastOrSmash()),
     }, {
         //Exits
         Entrance(RR_ZORAS_RIVER,  []{return logic->IsAdult || logic->BlastOrSmash();}),
@@ -58,6 +62,28 @@ void RegionTable_Init_ZoraRiver() {
         LOCATION(RC_ZR_BENEATH_WATERFALL_MIDDLE_LEFT_RUPEE,  logic->IsAdult && (logic->HasItem(RG_BRONZE_SCALE) || logic->CanUse(RG_IRON_BOOTS) || logic->CanUse(RG_BOOMERANG))),
         LOCATION(RC_ZR_BENEATH_WATERFALL_MIDDLE_RIGHT_RUPEE, logic->IsAdult && (logic->HasItem(RG_BRONZE_SCALE) || logic->CanUse(RG_IRON_BOOTS) || logic->CanUse(RG_BOOMERANG))),
         LOCATION(RC_ZR_BENEATH_WATERFALL_RIGHT_RUPEE,        logic->IsAdult && (logic->HasItem(RG_BRONZE_SCALE) || logic->CanUse(RG_IRON_BOOTS) || logic->CanUse(RG_BOOMERANG))),
+        LOCATION(RC_ZR_CIRCLE_ROCK_1,                        true),
+        LOCATION(RC_ZR_CIRCLE_ROCK_2,                        true),
+        LOCATION(RC_ZR_CIRCLE_ROCK_3,                        true),
+        LOCATION(RC_ZR_CIRCLE_ROCK_4,                        true),
+        LOCATION(RC_ZR_CIRCLE_ROCK_5,                        true),
+        LOCATION(RC_ZR_CIRCLE_ROCK_6,                        true),
+        LOCATION(RC_ZR_CIRCLE_ROCK_7,                        true),
+        LOCATION(RC_ZR_CIRCLE_ROCK_8,                        true),
+        LOCATION(RC_ZR_UPPER_CIRCLE_BOULDER,            logic->BlastOrSmash()),
+        LOCATION(RC_ZR_UPPER_CIRCLE_ROCK_1,                  true),
+        LOCATION(RC_ZR_UPPER_CIRCLE_ROCK_2,                  true),
+        LOCATION(RC_ZR_UPPER_CIRCLE_ROCK_3,                  true),
+        LOCATION(RC_ZR_UPPER_CIRCLE_ROCK_4,                  true),
+        LOCATION(RC_ZR_UPPER_CIRCLE_ROCK_5,                  true),
+        LOCATION(RC_ZR_UPPER_CIRCLE_ROCK_6,                  true),
+        LOCATION(RC_ZR_UPPER_CIRCLE_ROCK_7,                  true),
+        LOCATION(RC_ZR_UPPER_CIRCLE_ROCK_8,                  true),
+        LOCATION(RC_ZR_ROCK,                                 true),
+        LOCATION(RC_ZR_UNDERWATER_ROCK_1,                    logic->IsAdult || logic->CanUse(RG_BOMBCHU_5)),
+        LOCATION(RC_ZR_UNDERWATER_ROCK_2,                    logic->IsAdult || logic->CanUse(RG_BOMBCHU_5)),
+        LOCATION(RC_ZR_UNDERWATER_ROCK_3,                    logic->IsAdult || logic->CanUse(RG_BOMBCHU_5)),
+        LOCATION(RC_ZR_UNDERWATER_ROCK_4,                    logic->IsAdult || logic->CanUse(RG_BOMBCHU_5)),
         LOCATION(RC_ZR_NEAR_GROTTOS_GOSSIP_STONE,            true),
         LOCATION(RC_ZR_NEAR_DOMAIN_GOSSIP_STONE,             true),
         LOCATION(RC_ZR_NEAR_FREESTANDING_POH_GRASS,          logic->CanCutShrubs()),

serprex/Shipwright

soh/soh/Enhancements/randomizer/location_access/overworld/zoras_river.cpp
@@ -20,6 +20,10 @@ void RegionTable_Init_ZoraRiver() {
         LOCATION(RC_ZR_GRASS_10,             logic->CanCutShrubs()),
         LOCATION(RC_ZR_GRASS_11,             logic->CanCutShrubs()),
         LOCATION(RC_ZR_GRASS_12,             logic->CanCutShrubs()),
+        LOCATION(RC_ZR_BOULDER_1,            logic->IsChild && logic->BlastOrSmash()),
+        LOCATION(RC_ZR_BOULDER_2,            logic->IsChild && logic->BlastOrSmash()),
+        LOCATION(RC_ZR_BOULDER_3,            logic->IsChild && logic->BlastOrSmash()),
+        LOCATION(RC_ZR_BOULDER_4,            logic->IsChild && logic->BlastOrSmash()),
         LOCATION(RC_ZR_TREE,                 logic->IsChild && logic->CanBonkTrees()),
         // Require backflip with Iron Boots
         LOCATION(RC_ZR_WONDER_LOWER_RIVER_1, logic->IsChild && (logic->HasItem(RG_BRONZE_SCALE) || logic->CanUse(RG_IRON_BOOTS) || ctx->GetTrickOption(RT_VOIDOUT_COLLECTION))),
@@ -59,7 +63,19 @@ void RegionTable_Init_ZoraRiver() {
         LOCATION(RC_ZR_BENEATH_WATERFALL_MIDDLE_LEFT_RUPEE,  logic->IsAdult && (logic->HasItem(RG_BRONZE_SCALE) || logic->CanUse(RG_IRON_BOOTS) || logic->CanUse(RG_BOOMERANG) || ctx->GetTrickOption(RT_VOIDOUT_COLLECTION))),
         LOCATION(RC_ZR_BENEATH_WATERFALL_MIDDLE_RIGHT_RUPEE, logic->IsAdult && (logic->HasItem(RG_BRONZE_SCALE) || logic->CanUse(RG_IRON_BOOTS) || logic->CanUse(RG_BOOMERANG) || ctx->GetTrickOption(RT_VOIDOUT_COLLECTION))),
         LOCATION(RC_ZR_BENEATH_WATERFALL_RIGHT_RUPEE,        logic->IsAdult && (logic->HasItem(RG_BRONZE_SCALE) || logic->CanUse(RG_IRON_BOOTS) || logic->CanUse(RG_BOOMERANG) || ctx->GetTrickOption(RT_VOIDOUT_COLLECTION))),
-        LOCATION(RC_ZR_NEAR_DOMAIN_GOSSIP_STONE,             true),
+        LOCATION(RC_ZR_CIRCLE_ROCK_1,                        logic->CanBreakRocks()),
+        LOCATION(RC_ZR_CIRCLE_ROCK_2,                        logic->CanBreakRocks()),
+        LOCATION(RC_ZR_CIRCLE_ROCK_3,                        logic->CanBreakRocks()),
+        LOCATION(RC_ZR_CIRCLE_ROCK_4,                        logic->CanBreakRocks()),
+        LOCATION(RC_ZR_CIRCLE_ROCK_5,                        logic->CanBreakRocks()),
+        LOCATION(RC_ZR_CIRCLE_ROCK_6,                        logic->CanBreakRocks()),
+        LOCATION(RC_ZR_CIRCLE_ROCK_7,                        logic->CanBreakRocks()),
+        LOCATION(RC_ZR_CIRCLE_ROCK_8,                        logic->CanBreakRocks()),
+        LOCATION(RC_ZR_ROCK,                                 logic->CanBreakRocks()),
+        LOCATION(RC_ZR_UNDERWATER_ROCK_1,                    logic->CanUse(RG_BOMBCHU_5) || (logic->IsAdult && logic->CanBreakRocks())),
+        LOCATION(RC_ZR_UNDERWATER_ROCK_2,                    logic->CanUse(RG_BOMBCHU_5) || (logic->IsAdult && logic->CanBreakRocks())),
+        LOCATION(RC_ZR_UNDERWATER_ROCK_3,                    logic->CanUse(RG_BOMBCHU_5) || (logic->IsAdult && logic->CanBreakRocks())),
+        LOCATION(RC_ZR_UNDERWATER_ROCK_4,                    logic->CanUse(RG_BOMBCHU_5) || (logic->IsAdult && logic->CanBreakRocks())),
         LOCATION(RC_ZR_NEAR_FREESTANDING_POH_GRASS,          logic->CanUse(RG_BOOMERANG)),
         LOCATION(RC_ZR_NEAR_ROCK_CIRCLE_BUTTERFLY_FAIRY,     logic->IsChild && logic->CanUse(RG_STICKS)),
         LOCATION(RC_ZR_WATERFALL_BUTTERFLY_FAIRY,            logic->IsChild && logic->CanUse(RG_STICKS)),
@@ -91,6 +107,7 @@ void RegionTable_Init_ZoraRiver() {
         LOCATION(RC_ZR_WONDER_NEAR_CUCCO_1,                  logic->IsChild && (logic->HasItem(RG_BRONZE_SCALE) || logic->CanUse(RG_IRON_BOOTS) || ctx->GetTrickOption(RT_VOIDOUT_COLLECTION))), // Requires backflip with Iron Boots
         LOCATION(RC_ZR_WONDER_NEAR_CUCCO_2,                  logic->IsChild && (logic->HasItem(RG_BRONZE_SCALE) || logic->CanUse(RG_IRON_BOOTS) || ctx->GetTrickOption(RT_VOIDOUT_COLLECTION))), // Requires backflip with Iron Boots
         LOCATION(RC_ZR_WONDER_NEAR_CUCCO_3,                  logic->IsChild && (logic->HasItem(RG_BRONZE_SCALE) || logic->CanUse(RG_IRON_BOOTS) || ctx->GetTrickOption(RT_VOIDOUT_COLLECTION))),
+        LOCATION(RC_ZR_NEAR_DOMAIN_GOSSIP_STONE,             true),
     }, {
         //Exits
         ENTRANCE(RR_ZR_FRONT,            logic->IsAdult || logic->HasItem(RG_BRONZE_SCALE) || logic->HasItem(RG_POWER_BRACELET) || logic->BlastOrSmash() || logic->HasItem(RG_HOVER_BOOTS)),
@@ -109,6 +126,15 @@ void RegionTable_Init_ZoraRiver() {
         LOCATION(RC_ZR_GS_NEAR_RAISED_GROTTOS,              logic->IsAdult && logic->CanGetEnemyDrop(RE_GOLD_SKULLTULA, ED_BOOMERANG) && logic->CanGetNightTimeGS()),
         LOCATION(RC_ZR_NEAR_GROTTOS_GOSSIP_STONE_FAIRY,     logic->CallGossipFairy()),
         LOCATION(RC_ZR_NEAR_GROTTOS_GOSSIP_STONE_FAIRY_BIG, logic->CanUse(RG_SONG_OF_STORMS)),
+        LOCATION(RC_ZR_UPPER_CIRCLE_BOULDER,                logic->BlastOrSmash()),
+        LOCATION(RC_ZR_UPPER_CIRCLE_ROCK_1,                 logic->CanBreakRocks()),
+        LOCATION(RC_ZR_UPPER_CIRCLE_ROCK_2,                 logic->CanBreakRocks()),
+        LOCATION(RC_ZR_UPPER_CIRCLE_ROCK_3,                 logic->CanBreakRocks()),
+        LOCATION(RC_ZR_UPPER_CIRCLE_ROCK_4,                 logic->CanBreakRocks()),
+        LOCATION(RC_ZR_UPPER_CIRCLE_ROCK_5,                 logic->CanBreakRocks()),
+        LOCATION(RC_ZR_UPPER_CIRCLE_ROCK_6,                 logic->CanBreakRocks()),
+        LOCATION(RC_ZR_UPPER_CIRCLE_ROCK_7,                 logic->CanBreakRocks()),
+        LOCATION(RC_ZR_UPPER_CIRCLE_ROCK_8,                 logic->CanBreakRocks()),
         LOCATION(RC_ZR_NEAR_GROTTOS_GOSSIP_STONE,           true),
     }, {
         //Exits