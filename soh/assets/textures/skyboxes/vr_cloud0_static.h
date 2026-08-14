2f39b7d4b852b592e16892451fbdc533f428640f
louist103 <35883445+louist103@users.noreply.github.com>
2026-08-14 18:04:20 -0400

    Add symlinked files and delete dups

--- soh/assets/textures/skyboxes/vr_cloud0_static.h
+++ soh/assets/textures/skyboxes/vr_cloud0_static.h
@@ -3,19 +3,19 @@
 
 #include "align_asset_macro.h"
 
-#define dgSunriseOvercastSkybox1Tex "__OTR__textures/gSunriseOvercastSkybox1Tex"
+#define dgSunriseOvercastSkybox1Tex "__OTR__textures/skyboxes/gSunriseOvercastSkybox1Tex"
 static const ALIGN_ASSET(2) char gSunriseOvercastSkybox1Tex[] = dgSunriseOvercastSkybox1Tex;
 
-#define dgSunriseOvercastSkybox2Tex "__OTR__textures/gSunriseOvercastSkybox2Tex"
+#define dgSunriseOvercastSkybox2Tex "__OTR__textures/skyboxes/gSunriseOvercastSkybox2Tex"
 static const ALIGN_ASSET(2) char gSunriseOvercastSkybox2Tex[] = dgSunriseOvercastSkybox2Tex;
 
-#define dgSunriseOvercastSkybox3Tex "__OTR__textures/gSunriseOvercastSkybox3Tex"
+#define dgSunriseOvercastSkybox3Tex "__OTR__textures/skyboxes/gSunriseOvercastSkybox3Tex"
 static const ALIGN_ASSET(2) char gSunriseOvercastSkybox3Tex[] = dgSunriseOvercastSkybox3Tex;
 
-#define dgSunriseOvercastSkybox4Tex "__OTR__textures/gSunriseOvercastSkybox4Tex"
+#define dgSunriseOvercastSkybox4Tex "__OTR__textures/skyboxes/gSunriseOvercastSkybox4Tex"
 static const ALIGN_ASSET(2) char gSunriseOvercastSkybox4Tex[] = dgSunriseOvercastSkybox4Tex;
 
-#define dgSunriseOvercastSkybox5Tex "__OTR__textures/gSunriseOvercastSkybox5Tex"
+#define dgSunriseOvercastSkybox5Tex "__OTR__textures/skyboxes/gSunriseOvercastSkybox5Tex"
 static const ALIGN_ASSET(2) char gSunriseOvercastSkybox5Tex[] = dgSunriseOvercastSkybox5Tex;
 
 #endif // TEXTURES_VR_CLOUD0_STATIC_H
